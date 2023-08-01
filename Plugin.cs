88782c35027c608a2d170d15e1c34e060740b011
space-commits <57108281+space-commits@users.noreply.github.com>
2023-08-01 18:59:20 +0100

    removed junk + updated to 3.6.0

--- Plugin.cs
+++ Plugin.cs
@@ -10,34 +10,12 @@ namespace Bridge
 
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     [BepInDependency("FOVFix", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInDependency("RealismMod", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("RecoilStandalone", BepInDependency.DependencyFlags.HardDependency)]
     public class Plugin : BaseUnityPlugin
     {
-
-        public bool CheckIsReady()
-        {
-            var gameWorld = Singleton<GameWorld>.Instance;
-            var sessionResultPanel = Singleton<SessionResultPanel>.Instance;
-
-            if (gameWorld == null || gameWorld.AllPlayers == null || gameWorld.AllPlayers.Count <= 0 || sessionResultPanel != null)
-            {
-                return false;
-            }
-            return true;
-        }
-
         void Update()
         {
-            RealismMod.Plugin.StartingAimSens = FOVFix.Plugin.AimingSens;
-            RealismMod.Plugin.IsInDeadZone = FOVFix.FreeAimController.IsInDeadZone;
-            RealismMod.Plugin.DeadZoneReduceSens = !FOVFix.Plugin.FreeAimBlocksRotation.Value;
-            RealismMod.Plugin.DeadZoneSensAmount = FOVFix.Plugin.FreeAimRotationReduction.Value;
-            RealismMod.Plugin.DeadZonePanCamUp = FOVFix.FreeAimController.PanCamUp;
-            RealismMod.Plugin.DeadZonePanCamDown = FOVFix.FreeAimController.PanCamDown;
-            RealismMod.Plugin.DeadZonePanCamLeft = FOVFix.FreeAimController.PanCamLeft;
-            RealismMod.Plugin.DeadZonePanCamRight = FOVFix.FreeAimController.PanCamRight;
-            RealismMod.Plugin.FreeAimEnabled = FOVFix.Plugin.EnableFreeAim.Value;
-            RealismMod.Plugin.CamRotationMulti = FOVFix.Plugin.CamRotationMulti.Value;
+            RecoilStandalone.Plugin.StartingAimSens = FOVFix.Plugin.AimingSens;
         }
     }
 }
